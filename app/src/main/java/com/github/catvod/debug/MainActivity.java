81e4e9d0ee310cda2fdd970934a3041b66e1ba7d
FongMi <b8892778@gmail.com>
2022-08-22 15:27:30 +0800

    Add debug package

--- app/src/main/java/com/github/catvod/debug/MainActivity.java
+++ app/src/main/java/com/github/catvod/debug/MainActivity.java
@@ -1,7 +1,9 @@
-package com.github.catvod;
+package com.github.catvod.debug;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.github.catvod.R;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(() -> {
+
+        }).start();
     }
 }
