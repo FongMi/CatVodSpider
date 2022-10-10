39df51a8f7ea66795a73e865a3a67c53a7582392
FongMi <b8892778@gmail.com>
2022-10-10 18:43:20 +0800

    Add dolll

--- app/src/main/java/com/github/catvod/spider/Doll.java
+++ app/src/main/java/com/github/catvod/spider/Doll.java
@@ -1,7 +1,21 @@
 package com.github.catvod.spider;
 
-import com.github.catvod.crawler.Spider;
+import android.os.SystemClock;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import com.github.catvod.bean.Class;
+import com.github.catvod.bean.Result;
+import com.github.catvod.bean.Vod;
+import com.github.catvod.crawler.Spider;
+import com.github.catvod.net.OkHttpUtil;
+import com.github.catvod.utils.Misc;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,26 +25,70 @@ public class Doll extends Spider {
 
     @Override
     public String homeContent(boolean filter) throws Exception {
-        return super.homeContent(filter);
+        List<Class> classes = new ArrayList<>();
+        List<Vod> list = new ArrayList<>();
+        Document doc = Jsoup.parse(OkHttpUtil.string(url));
+        for (Element a : doc.select("ul#side-menu > li > a")) {
+            String typeName = a.text();
+            String typeId = a.attr("href").replace(url, "");
+            classes.add(new Class(typeId, typeName));
+        }
+        for (Element div : doc.select("div.video-detail")) {
+            String id = div.select("h3.video-title > a").attr("href").replace(url, "");
+            String name = div.select("h3.video-title > a").text();
+            String pic = url + div.select("div.thumb > a > img").attr("data-src");
+            String remark = div.select("div.date").text();
+            list.add(new Vod(id, name, pic, remark));
+        }
+        return Result.string(classes, list);
     }
 
     @Override
     public String categoryContent(String tid, String pg, boolean filter, HashMap<String, String> extend) throws Exception {
-        return super.categoryContent(tid, pg, filter, extend);
+        List<Vod> list = new ArrayList<>();
+        String target = pg.equals("1") ? url + tid : url + tid + "/" + pg + ".html";
+        Document doc = Jsoup.parse(OkHttpUtil.string(target));
+        for (Element div : doc.select("div.video-detail")) {
+            String id = div.select("h3.video-title > a").attr("href").replace(url, "");
+            String name = div.select("h3.video-title > a").text();
+            String pic = url + div.select("div.thumb > a > img").attr("data-src");
+            String remark = div.select("div.date").text();
+            list.add(new Vod(id, name, pic, remark));
+        }
+        return Result.string(list);
     }
 
     @Override
     public String detailContent(List<String> ids) throws Exception {
-        return super.detailContent(ids);
-    }
-
-    @Override
-    public String searchContent(String key, boolean quick) throws Exception {
-        return super.searchContent(key, quick);
+        Document doc = Jsoup.parse(OkHttpUtil.string(url + ids.get(0)));
+        String name = doc.select("meta[property=og:title]").attr("content");
+        String pic = doc.select("meta[property=og:image]").attr("content");
+        Vod vod = new Vod();
+        vod.setVodId(ids.get(0));
+        vod.setVodPic(pic);
+        vod.setVodName(name);
+        vod.setVodPlayFrom("玩偶姐姐");
+        vod.setVodPlayUrl("播放$" + ids.get(0));
+        return Result.string(vod);
     }
 
     @Override
     public String playerContent(String flag, String id, List<String> vipFlags) throws Exception {
-        return super.playerContent(flag, id, vipFlags);
+        HashMap<String, String> result = new HashMap<>();
+        Misc.loadWebView(url + id, getClient(result));
+        while (result.isEmpty()) SystemClock.sleep(10);
+        return Result.get().url(result.get("url")).string();
+    }
+
+    private WebViewClient getClient(HashMap<String, String> result) {
+        return new WebViewClient() {
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                if (url.endsWith(".m3u8")) {
+                    result.put("url", url);
+                    view.destroy();
+                }
+            }
+        };
     }
 }
