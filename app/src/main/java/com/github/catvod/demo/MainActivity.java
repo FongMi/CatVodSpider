eaa98413a81d7a160aec00607307a34db7c7c9bd
FongMi <b8892778@gmail.com>
2022-11-06 15:21:52 +0800

    Update MainActivity.java

--- app/src/main/java/com/github/catvod/demo/MainActivity.java
+++ app/src/main/java/com/github/catvod/demo/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.github.catvod.R;
+import com.github.catvod.spider.Init;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +12,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Init.init(getApplicationContext());
         new Thread(() -> {
+            
         }).start();
     }
 }
