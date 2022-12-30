c87e0b720774093a42e07fb58b1b6f2dbe94814e
FongMi <b8892778@gmail.com>
2022-12-30 16:13:55 +0800

    Clean code

--- app/src/main/java/com/github/catvod/net/Cronet.java
+++ app/src/main/java/com/github/catvod/net/Cronet.java
@@ -1,38 +1,38 @@
 package com.github.catvod.net;
 
 import com.github.catvod.crawler.Spider;
+import com.github.catvod.spider.Init;
+import com.google.net.cronet.okhttptransport.CronetInterceptor;
+
+import org.chromium.net.CronetEngine;
 
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.Call;
 import okhttp3.Dns;
 import okhttp3.OkHttpClient;
 
-public class OkHttp {
-
-    public static final String POST = "POST";
-    public static final String GET = "GET";
+public class Cronet {
 
     private final OkHttpClient noRedirect;
     private final OkHttpClient client;
 
     private static class Loader {
-        static volatile OkHttp INSTANCE = new OkHttp();
+        static volatile Cronet INSTANCE = new Cronet();
     }
 
-    public static OkHttp get() {
+    public static Cronet get() {
         return Loader.INSTANCE;
     }
 
-    public OkHttp() {
+    public Cronet() {
         client = getBuilder().build();
         noRedirect = client.newBuilder().followRedirects(false).followSslRedirects(false).build();
     }
 
     private OkHttpClient.Builder getBuilder() {
-        return new OkHttpClient.Builder().dns(safeDns()).callTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).writeTimeout(30, TimeUnit.SECONDS).connectTimeout(30, TimeUnit.SECONDS).retryOnConnectionFailure(true).hostnameVerifier(SSLSocketFactoryCompat.hostnameVerifier).sslSocketFactory(new SSLSocketFactoryCompat(), SSLSocketFactoryCompat.trustAllCert);
+        return new OkHttpClient.Builder().dns(safeDns()).addInterceptor(CronetInterceptor.newBuilder(new CronetEngine.Builder(Init.context()).build()).build()).callTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).writeTimeout(30, TimeUnit.SECONDS).connectTimeout(30, TimeUnit.SECONDS).retryOnConnectionFailure(true).hostnameVerifier(SSLSocketFactoryCompat.hostnameVerifier).sslSocketFactory(new SSLSocketFactoryCompat(), SSLSocketFactoryCompat.trustAllCert);
     }
 
     private static OkHttpClient client() {
@@ -52,7 +52,7 @@ public class OkHttp {
     }
 
     public static void stringNoRedirect(String url, Map<String, String> header, Map<String, List<String>> respHeader) {
-        string(noRedirect(), GET, url, null, null, header, respHeader);
+        string(noRedirect(), OkHttp.GET, url, null, null, header, respHeader);
     }
 
     public static String string(OkHttpClient client, String method, String url, String tag, Map<String, String> params, Map<String, String> header, Map<String, List<String>> respHeader) {
@@ -80,7 +80,7 @@ public class OkHttp {
     }
 
     public static String string(String url, String tag, Map<String, String> params, Map<String, String> header, Map<String, List<String>> respHeader) {
-        return string(client(), GET, url, tag, params, header, respHeader);
+        return string(client(), OkHttp.GET, url, tag, params, header, respHeader);
     }
 
     public static String post(String url, Map<String, String> params) {
@@ -92,7 +92,7 @@ public class OkHttp {
     }
 
     public static String post(String url, Map<String, String> params, Map<String, String> header, Map<String, List<String>> respHeader) {
-        return string(client(), POST, url, null, params, header, respHeader);
+        return string(client(), OkHttp.POST, url, null, params, header, respHeader);
     }
 
     public static String postJson(String url, String json) {
@@ -100,20 +100,7 @@ public class OkHttp {
     }
 
     public static String postJson(String url, String json, Map<String, String> header) {
-        return new OkRequest(POST, url, json, header).execute(client());
-    }
-
-    public static void cancel(Object tag) {
-        for (Call call : client().dispatcher().queuedCalls()) {
-            if (tag.equals(call.request().tag())) {
-                call.cancel();
-            }
-        }
-        for (Call call : client().dispatcher().runningCalls()) {
-            if (tag.equals(call.request().tag())) {
-                call.cancel();
-            }
-        }
+        return new OkRequest(OkHttp.POST, url, json, header).execute(client());
     }
 
     public static String getRedirectLocation(Map<String, List<String>> headers) {
