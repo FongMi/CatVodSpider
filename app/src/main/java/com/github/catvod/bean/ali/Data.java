bf2c9e501ae5224afdb37a8c5ed97257d588cebc
FongMi <b8892778@gmail.com>
2023-02-08 01:33:02 +0800

    Update ali qrcode token

--- app/src/main/java/com/github/catvod/bean/ali/Data.java
+++ app/src/main/java/com/github/catvod/bean/ali/Data.java
@@ -1,24 +1,31 @@
 package com.github.catvod.bean.ali;
 
+import android.util.Base64;
+
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
 public class Data {
 
-    @SerializedName(value = "data", alternate = "pds_login_result")
+    @SerializedName("data")
     private Data data;
+    @SerializedName("content")
+    private Data content;
     @SerializedName("t")
     private String t;
     @SerializedName("ck")
     private String ck;
     @SerializedName("codeContent")
     private String codeContent;
-    @SerializedName("refreshToken")
-    private String refreshToken;
+    @SerializedName("qrCodeStatus")
+    private String qrCodeStatus;
+    @SerializedName("bizExt")
+    private String bizExt;
 
     public static Data objectFrom(String str) {
         try {
-            return new Gson().fromJson(str, Data.class);
+            Data data = new Gson().fromJson(str, Data.class);
+            return data == null ? new Data() : data;
         } catch (Exception e) {
             return new Data();
         }
@@ -28,8 +35,8 @@ public class Data {
         return data == null ? new Data() : data;
     }
 
-    public boolean hasToken() {
-        return getData().getRefreshToken().length() > 0;
+    public Data getContent() {
+        return content == null ? new Data() : content;
     }
 
     public String getT() {
@@ -44,7 +51,23 @@ public class Data {
         return codeContent == null ? "" : codeContent;
     }
 
-    public String getRefreshToken() {
-        return refreshToken == null ? "" : refreshToken;
+    public String getQrCodeStatus() {
+        return qrCodeStatus == null ? "" : qrCodeStatus;
+    }
+
+    public String getBizExt() {
+        return bizExt == null ? "" : bizExt;
+    }
+
+    public String getToken() {
+        return Biz.objectFrom(new String(Base64.decode(getBizExt(), Base64.DEFAULT))).getPdsLoginResult().getRefreshToken();
+    }
+
+    public boolean hasToken() {
+        return getQrCodeStatus().equals("CONFIRMED") && getBizExt().length() > 0;
+    }
+
+    public boolean isExpired() {
+        return getQrCodeStatus().equals("EXPIRED");
     }
 }
