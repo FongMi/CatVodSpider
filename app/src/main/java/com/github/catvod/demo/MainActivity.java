51aba34e35a9d05ce65c82e0de2cfdb4429f891c
FongMi <b8892778@gmail.com>
2023-02-16 15:11:04 +0800

    Waiting

--- app/src/main/java/com/github/catvod/demo/MainActivity.java
+++ app/src/main/java/com/github/catvod/demo/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.github.catvod.R;
+import com.github.catvod.spider.Ali;
 import com.github.catvod.spider.Init;
 
 public class MainActivity extends Activity {
@@ -14,7 +15,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Init.init(getApplicationContext());
         new Thread(() -> {
-
+            Ali ali = new Ali();
+            ali.init("https://agit.ai/Yoursmile7/TVBox/raw/branch/master/token.txt");
+            ali.playerContent("普畫", "63de1c59a77dc671045c4a35b42e96d5621f4dc0");
         }).start();
     }
 }
