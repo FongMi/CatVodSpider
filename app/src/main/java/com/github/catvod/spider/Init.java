a8cfeba8bc1a8c894a21a0047bcc062784e0ab5d
FongMi <b8892778@gmail.com>
2023-03-01 16:52:35 +0800

    Revert

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -8,7 +8,6 @@ import android.os.Looper;
 import android.widget.Toast;
 
 import com.github.catvod.crawler.SpiderDebug;
-import com.github.catvod.hook.Ctx;
 import com.github.catvod.utils.Trans;
 
 import java.lang.reflect.Field;
@@ -37,10 +36,6 @@ public class Init {
 
     public static Application context() {
         return get().app;
-    }
-
-    public static Context fakeCtx() {
-        return new Ctx(get().app);
     }
 
     public static void init(Context context) {
