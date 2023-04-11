824fc26fdad361ad31b86273f5b71484f53f52b2
唐三 <89683694+Tangsan99999@users.noreply.github.com>
2023-04-11 18:55:14 +0800

    蛋疼

--- app/src/main/java/com/github/catvod/spider/Bili.java
+++ app/src/main/java/com/github/catvod/spider/Bili.java
@@ -75,8 +75,8 @@ public class Bili extends Spider {
             }
             ext = new JSONObject(extend);
             if (ext.optString("cookie").length() > 0) {
-                if (ext.optString("cookie").startsWith("http")) cookie = OkHttp.string(ext.optString("cookie"));
                 cookie = ext.optString("cookie");
+                if (cookie.startsWith("http")) cookie = OkHttp.string(cookie);
             } else {
                 getCookie();
             }
