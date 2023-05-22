f4a40e92b33502c5729f2fcfbcedcc3cae183e3f
FongMi <b8892778@gmail.com>
2023-05-22 14:58:20 +0800

    Fix alist token header

--- app/src/main/java/com/github/catvod/bean/alist/Drive.java
+++ app/src/main/java/com/github/catvod/bean/alist/Drive.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import com.github.catvod.bean.Class;
 import com.github.catvod.net.OkHttp;
 import com.github.catvod.utils.Image;
+import com.github.catvod.utils.Utils;
 import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
@@ -32,6 +33,8 @@ public class Drive {
     private int version;
     @SerializedName("path")
     private String path;
+    @SerializedName("token")
+    private String token;
 
     public static Drive objectFrom(String str) {
         return new Gson().fromJson(str, Drive.class);
@@ -79,6 +82,14 @@ public class Drive {
 
     public void setPath(String path) {
         this.path = TextUtils.isEmpty(path) ? "" : path;
+    }
+
+    public String getToken() {
+        return TextUtils.isEmpty(token) ? "" : token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 
     public boolean isNew() {
@@ -133,6 +144,13 @@ public class Drive {
             params.put("path", "/");
             return new Gson().toJson(params);
         }
+    }
+
+    public HashMap<String, String> getHeader() {
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("User-Agent", Utils.CHROME);
+        if (!getToken().isEmpty()) headers.put("Authorization", token);
+        return headers;
     }
 
     @Override
