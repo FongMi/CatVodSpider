eb442cc928e6a2c24d5593c9eeddd7dd076187d4
FongMi <b8892778@gmail.com>
2023-08-08 16:16:31 +0800

    Update

--- app/src/main/java/com/github/catvod/net/OkHttp.java
+++ app/src/main/java/com/github/catvod/net/OkHttp.java
@@ -36,7 +36,7 @@ public class OkHttp {
     }
 
     public static OkHttpClient.Builder getBuilder() {
-        return new OkHttpClient.Builder().dns(safeDns()).readTimeout(30, TimeUnit.SECONDS).writeTimeout(30, TimeUnit.SECONDS).connectTimeout(30, TimeUnit.SECONDS).hostnameVerifier(SSLSocketFactoryCompat.hostnameVerifier).sslSocketFactory(new SSLSocketFactoryCompat(), SSLSocketFactoryCompat.trustAllCert);
+        return new OkHttpClient.Builder().dns(safeDns()).readTimeout(30, TimeUnit.SECONDS).writeTimeout(30, TimeUnit.SECONDS).connectTimeout(30, TimeUnit.SECONDS).sslSocketFactory(SSLCompat.get(), SSLCompat.TM);
     }
 
     public static OkHttpClient client() {
