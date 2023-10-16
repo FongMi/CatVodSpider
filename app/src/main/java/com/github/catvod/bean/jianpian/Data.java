cae3e20af79718fbcf5816e6ecedb6641310f5cf
FongMi <b8892778@gmail.com>
2023-10-16 14:46:51 +0800

    Clean code

--- app/src/main/java/com/github/catvod/bean/jianpian/Data.java
+++ app/src/main/java/com/github/catvod/bean/jianpian/Data.java
@@ -6,7 +6,6 @@ import com.github.catvod.bean.Vod;
 import com.github.catvod.utils.Utils;
 import com.google.gson.annotations.SerializedName;
 
-import java.util.Collections;
 import java.util.List;
 
 public class Data {
@@ -34,7 +33,7 @@ public class Data {
     @SerializedName("directors")
     private List<Value> directors;
     @SerializedName("btbo_downlist")
-    private List<BtboDown> btboDownlist;
+    private List<Value> btboDownlist;
 
     public String getJumpId() {
         return TextUtils.isEmpty(jumpId) ? "" : jumpId;
@@ -53,74 +52,64 @@ public class Data {
     }
 
     public String getDescription() {
-        return TextUtils.isEmpty(description) ? "" : description;
+        return TextUtils.isEmpty(description) ? "" : description.replace("　", "");
     }
 
     public String getPlaylist() {
-        return playlist == null ? "" : playlist.getTitle();
+        return playlist == null ? "" : playlist.getValue();
     }
 
     public String getYear() {
-        return year == null ? "" : year.getTitle();
+        return year == null ? "" : year.getValue();
     }
 
     public String getArea() {
-        return area == null ? "" : area.getTitle();
+        return area == null ? "" : area.getValue();
     }
 
     public String getTypes() {
-        return types == null ? "" : getValues(types);
+        return types == null ? "" : getValues(types, false, " ");
     }
 
     public String getActors() {
-        return actors == null ? "" : getValues(actors);
+        return actors == null ? "" : getValues(actors, true, " ");
     }
 
     public String getDirectors() {
-        return directors == null ? "" : getValues(directors);
+        return directors == null ? "" : getValues(directors, true, " ");
     }
 
-    public List<BtboDown> getBtboDownlist() {
-        return btboDownlist == null ? Collections.emptyList() : btboDownlist;
+    public String getPlayUrl() {
+        return btboDownlist == null ? "" : getValues(btboDownlist, false, "#");
     }
 
     public Vod vod() {
         return new Vod(getJumpId(), getTitle(), getThumbnail(), getMask());
     }
 
-    public String getPlayUrl() {
+    public String getValues(List<Value> items, boolean link, String join) {
         StringBuilder sb = new StringBuilder();
-        for (BtboDown value : getBtboDownlist()) sb.append(value.getVal()).append("#");
-        return Utils.substring(sb.toString());
-    }
-
-    public String getValues(List<Value> items) {
-        StringBuilder sb = new StringBuilder();
-        for (Value value : items) sb.append(value.getLink()).append(" ");
+        for (Value value : items) sb.append(value.getValue(link)).append(join);
         return Utils.substring(sb.toString());
     }
 
     public static class Value {
 
-        @SerializedName(value = "title", alternate = "name")
-        private String title;
+        @SerializedName(value = "val", alternate = {"name", "title"})
+        private String value;
 
-        public String getTitle() {
-            return TextUtils.isEmpty(title) ? "" : title;
+        private String getValue() {
+            if (TextUtils.isEmpty(value)) return "";
+            if (value.startsWith("ftp://")) return "tvbox-xg:" + value;
+            return value;
         }
 
-        public String getLink() {
-            return String.format("[a=cr:{\"id\":\"%s\",\"name\":\"%s\"}/]%s[/a]", getTitle() + "/{pg}", getTitle(), getTitle());
+        private String getLink() {
+            return String.format("[a=cr:{\"id\":\"%s\",\"name\":\"%s\"}/]%s[/a]", getValue() + "/{pg}", getValue(), getValue());
         }
-    }
 
-    public static class BtboDown {
-
-        @SerializedName("val")
-        private String val;
-
-        public String getVal() {
-            return TextUtils.isEmpty(val) ? "" : val.replaceAll("ftp", "tvbox-xg:ftp");
+        public String getValue(boolean link) {
+            return link ? getLink() : getValue();
         }
     }
 }
