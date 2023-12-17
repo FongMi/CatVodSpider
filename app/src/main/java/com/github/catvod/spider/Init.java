1b31eb04e11e181bf1ff29a512f3c0afe312cc4d
FongMi <b8892778@gmail.com>
2023-12-18 00:00:19 +0800

    Fix bug

--- app/src/main/java/com/github/catvod/spider/Init.java
+++ app/src/main/java/com/github/catvod/spider/Init.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
@@ -59,6 +60,7 @@ public class Init {
         try {
             Activity activity = Init.getActivity();
             if (activity == null || Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
+            if (activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) return;
             activity.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 9999);
         } catch (Exception e) {
             e.printStackTrace();
