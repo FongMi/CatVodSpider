c31ba00bb06684870368e00c42f54f5d3c738617
FongMi <b8892778@gmail.com>
2024-06-30 16:57:41 +0800

    Update gradle

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -12,17 +12,7 @@
 -keep class androidx.core.** { *; }
 
 # Gson
--keepattributes Signature
--keepattributes *Annotation*
--dontwarn sun.misc.**
 -keep class com.google.gson.** { *; }
--keep class * extends com.google.gson.TypeAdapter
--keep class * implements com.google.gson.TypeAdapterFactory
--keep class * implements com.google.gson.JsonSerializer
--keep class * implements com.google.gson.JsonDeserializer
--keepclassmembers,allowobfuscation class * { @com.google.gson.annotations.SerializedName <fields>; }
--keep,allowobfuscation,allowshrinking class com.google.gson.reflect.TypeToken
--keep,allowobfuscation,allowshrinking class * extends com.google.gson.reflect.TypeToken
 
 # Nano
 -keep class fi.iki.elonen.** { *; }
@@ -42,6 +32,8 @@
 -keep class com.thegrizzlylabs.sardineandroid.** { *; }
 
 # Smbj
+-dontwarn org.xmlpull.v1.**
+-dontwarn android.content.res.**
 -keep class com.hierynomus.** { *; }
 -keep class net.engio.mbassy.** { *; }
 
--- app/src/main/java/com/github/catvod/api/AliYun.java
+++ app/src/main/java/com/github/catvod/api/AliYun.java
@@ -14,7 +14,6 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import com.github.catvod.BuildConfig;
 import com.github.catvod.bean.Result;
 import com.github.catvod.bean.Sub;
 import com.github.catvod.bean.Vod;
@@ -219,7 +218,7 @@ public class AliYun {
         JsonObject param = new JsonObject();
         param.addProperty("authorize", 1);
         param.addProperty("scope", "user:base,file:all:read,file:all:write");
-        String url = "https://open.aliyundrive.com/oauth/users/authorize?client_id=" + BuildConfig.CLIENT_ID + "&redirect_uri=https://alist.nn.ci/tool/aliyundrive/callback&scope=user:base,file:all:read,file:all:write&state=";
+        String url = "https://open.aliyundrive.com/oauth/users/authorize?client_id=76917ccccd4441c39457a04f6084fb2f&redirect_uri=https://alist.nn.ci/tool/aliyundrive/callback&scope=user:base,file:all:read,file:all:write&state=";
         String json = auth(url, param.toString(), true);
         return oauthRedirect(Code.objectFrom(json).getCode());
     }
