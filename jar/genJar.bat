7d07d77ecc7d32ee8a4949bf986b663152a50c15
FongMi <b8892778@gmail.com>
2024-11-20 00:00:46 +0800

    Revert

--- jar/genJar.bat
+++ jar/genJar.bat
@@ -6,12 +6,15 @@ rd /s/q "%~dp0\Smali_classes"
 java -jar "%~dp0\3rd\baksmali-2.5.2.jar" d "%~dp0\..\app\build\intermediates\dex\release\minifyReleaseWithR8\classes.dex" -o "%~dp0\Smali_classes"
 
 rd /s/q "%~dp0\spider.jar\smali\com\github\catvod\spider"
+rd /s/q "%~dp0\spider.jar\smali\com\google\gson"
 rd /s/q "%~dp0\spider.jar\smali\org\slf4j\"
 
 if not exist "%~dp0\spider.jar\smali\com\github\catvod\" md "%~dp0\spider.jar\smali\com\github\catvod\"
+if not exist "%~dp0\spider.jar\smali\com\google\gson\" md "%~dp0\spider.jar\smali\com\google\gson\"
 if not exist "%~dp0\spider.jar\smali\org\slf4j\" md "%~dp0\spider.jar\smali\org\slf4j\"
 
 move "%~dp0\Smali_classes\com\github\catvod\spider" "%~dp0\spider.jar\smali\com\github\catvod\"
+move "%~dp0\Smali_classes\com\google\gson" "%~dp0\spider.jar\smali\com\google\gson\"
 move "%~dp0\Smali_classes\org\slf4j" "%~dp0\spider.jar\smali\org\slf4j\"
 
 java -jar "%~dp0\3rd\apktool_2.4.1.jar" b "%~dp0\spider.jar" -c
