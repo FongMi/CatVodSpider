a36d7f5017dd2fd76020748b8808a1385c41c351
kvymin <106472307+Kvymin@users.noreply.github.com>
2025-07-01 20:17:27 +0800

    Update Jianpian.java

--- app/src/main/java/com/github/catvod/spider/Jianpian.java
+++ app/src/main/java/com/github/catvod/spider/Jianpian.java
@@ -24,7 +24,7 @@ import java.util.Map;
  */
 public class Jianpian extends Spider {
 
-    private final String siteUrl = "http://api2.rinhome.com";
+    private final String siteUrl = "http://4icnx7.qyjzlh.com/";
     private String extend;
 
     private Map<String, String> getHeader() {
